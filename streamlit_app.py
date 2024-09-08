16b3c0d48aff94642f6a36ae67213460498c66cb
mlisbao <mlisbao@hotmail.com>
2024-09-08 11:57:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ buttons = []
 emojis = ["😡","😢","😐","😏","😃"]
 st.title("Como voce está Agora?")
 for i in range(5):
-    buttons.append(st.button(emojis(str(i))))
+    buttons.append(st.button(emojis[str(i)]))
 for i, button in enumerate(buttons):
     if button:
         st.write(f"{i} button was clicked")
