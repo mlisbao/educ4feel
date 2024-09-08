10d3beea5a00dd003418980a514628b390ab0a51
mlisbao <mlisbao@hotmail.com>
2024-09-08 14:31:29 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,6 @@
 import streamlit as st
 
-st.markdown(
-    """
-<style>
-button.label {font-size: 116px}
-</style>
-""",
-    unsafe_allow_html=True,
-)
+st.markdown('<style>.big-font {font-size:116px !important;}</style>', unsafe_allow_html=True)
 
 buttons = []
 emojis = ["😡","😢","😐","😏","😃"]
@@ -16,10 +9,8 @@ st.title("Como voce está Agora?")
 cols = st.columns(5)
 
 for i, x in enumerate(cols):
-    buttons.append(x.button(label = emojis[i]))
+    buttons.append(x.button(label = f'<span class="big-font">emojis[i]</span>', key='big_font_button', help='Button with increased font size')
 
 for i, button in enumerate(buttons):
     if button:
         st.write(f"{i} button was clicked")
-
-
