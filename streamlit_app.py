55fa359106be0b183e3025ec4e432b3a6307d93d
mlisbao <mlisbao@hotmail.com>
2024-09-08 15:56:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.title("Como voce está Agora?")
 cols = st.columns(5)
 
 for i, x in enumerate(cols):
-    buttons.append(x.button('teste',key=i))
+    buttons.append(x.button(emojis[1],key=i))
 
 for i, button in enumerate(buttons):
     if button:
