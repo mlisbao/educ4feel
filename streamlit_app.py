cdaaadf92cfa1ac0f4ff6f7469073233dec186af
mlisbao <mlisbao@hotmail.com>
2024-09-08 12:30:11 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ cols = st.columns(5)
 for i, x in enumerate(cols):
     x.button(emojis[i])
 
-for i, button in enumerate(x):
+for i, button in enumerate(cols):
     if button:
         st.write(f"{i} button was clicked")
 
